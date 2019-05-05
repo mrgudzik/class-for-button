9b81a1837e4827c16b5692460a32a743cb357415
gudzik <mr.gudzik@ukr.net>
2019-05-05 15:16:02 +0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,110 @@
+window.onload = function (e) {
+
+  let content = document.getElementById('content');
+
+  let button = new Button({
+    text: 'Текст кнопи'
+  });
+
+  console.log(button);
+
+  content.appendChild(button._element);
+
+  window.button = button;
+
+}
+
+class Button {
+  constructor({
+    text,
+    primary = false,
+    accent = false,
+    raised = false,
+    compact = false,
+    danse = false,
+    disabled = false
+  }) {
+    this._element = null;
+    this._text = text;
+    this._primary = primary;
+    this._accent = accent;
+    this._raised = raised;
+    this._compact = compact;
+    this._danse = danse;
+    this._disabled = disabled;
+
+    this.init();
+  }
+
+  init() {
+    this._element = document.createElement('button');
+    this._element.className = 'mdc-button';
+    this._element.textContent = this._text;
+  }
+
+  get text() {
+    return this._text;
+  }
+
+  set text(value) {
+    this._text = value;
+    this._element.innerText = value;
+  }
+
+  get disabled() {
+    return this._disabled;
+  }
+
+  set disabled(value) {
+    this._disabled = value;
+    this._element.disabled = value;
+  }
+
+  get primary() {
+    return this._primary;
+  }
+  set primary(value) {
+    this.toggle('primary', value)
+  }
+
+  get accent() {
+    return this._primary;
+  }
+  set accent(value) {
+    this.toggle('accent', value)
+  }
+
+  get raised() {
+    return this._primary;
+  }
+  set raised(value) {
+    this.toggle('raised', value)
+  }
+
+  get compact() {
+    return this._primary;
+  }
+  set compact(value) {
+    this.toggle('compact', value)
+  }
+
+  get danse() {
+    return this._primary;
+  }
+  set danse(value) {
+    this.toggle('danse', value)
+  }
+
+  toggle(prop, value) {
+    let className = `mdc-button--${prop}`;
+
+    this[`_${prop}`] = value;
+
+    if (this[`_${prop}`]) {
+      this._element.classList.add(className);
+    } else {
+      this._element.classList.remove(className);
+    }
+  }
+
+}
